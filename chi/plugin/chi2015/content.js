0890b8b679f3d903d096172aafd53418bb43c75e
wondershow <zhanglei4reg@gmail.com>
2014-07-24 07:47:19 -0400

    able to get a user selected text(either drag or doubleclick)

--- chi/plugin/chi2015/content.js
+++ chi/plugin/chi2015/content.js
@@ -254,3 +254,41 @@ $("body").click(function(event){
 		   },
            data: JSON.stringify(jsonObj)
 	});*/
+
+
+
+
+
+    
+
+
+
+	/***The following code shows how to capture a user selected text**/
+	/*
+	if(!window.Kolich){
+		Kolich = {};
+	}
+
+	Kolich.Selector = {};
+	Kolich.Selector.getSelected = function(){
+		var t = '';
+		if(window.getSelection){
+			t = window.getSelection();
+		}else if(document.getSelection){
+			t = document.getSelection();
+		}else if(document.selection){
+			t = document.selection.createRange().text;
+		}
+		return t;
+	}
+
+	Kolich.Selector.mouseup = function(){
+		var st = Kolich.Selector.getSelected();
+		if(st!=''){
+			alert("You selected:\n"+st);
+		}
+	}
+
+	$(document).ready(function(){
+		$(document).bind("mouseup", Kolich.Selector.mouseup);
+	}); */
