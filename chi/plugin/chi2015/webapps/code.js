73ff57adaeb2b92110086088f2dd50fb6510ba79
wondershow <zhanglei4reg@gmail.com>
2014-08-07 16:33:43 -0400

    Now we are able to access a google web app so that we can draw the user mouse trajectory and user click map.

--- chi/plugin/chi2015/webapps/code.js
+++ chi/plugin/chi2015/webapps/code.js
@@ -34,9 +34,213 @@ function onOpen() {
 };
 
 
+function getDataTable() {
+  
+  var res = {};
+  var ss = SpreadsheetApp.openById(ScriptProperties.getProperty('active'));
+  var sheet = ss.getSheetByName("DATA");
+  
+  var dataClickPage = Charts.newDataTable()
+     .addColumn(Charts.ColumnType.NUMBER, "X")
+     .addColumn(Charts.ColumnType.NUMBER, "Y")
+  
+  var dataClickScreen = Charts.newDataTable()
+     .addColumn(Charts.ColumnType.NUMBER, "X")
+     .addColumn(Charts.ColumnType.NUMBER, "Y")
+  
+  var dataMousePage = Charts.newDataTable()
+     .addColumn(Charts.ColumnType.NUMBER, "X")
+     .addColumn(Charts.ColumnType.NUMBER, "Y")
+  
+  var dataMouseScreen = Charts.newDataTable()
+     .addColumn(Charts.ColumnType.NUMBER, "X")
+     .addColumn(Charts.ColumnType.NUMBER, "Y")
+  
+  
+  
+  var colName2IndexMapping = {};
+  var colNum = sheet.getLastColumn();
+  
+  var headers = sheet.getRange(1, 1, 1, colNum).getValues()[0]; //read headers
+  for(i=0;i<headers.length;i++) {
+    Logger.log(headers[i]);
+    colName2IndexMapping[headers[i]] = i;
+  }
+  
+  var range = sheet.getDataRange();
+  var lastRow = range.getLastRow();
+  
+  for(var i=2;i<=lastRow;i++)  {
+    row = sheet.getRange(i, 1, 1, colNum).getValues()[0];
+    var indexType = colName2IndexMapping['Type'];
+    var indexPageX = colName2IndexMapping['PageX'];
+    var indexPageY = colName2IndexMapping['PageY'];
+    var indexScreenX = colName2IndexMapping['ClientX'];
+    var indexScreenY = colName2IndexMapping['ClientY'];
+    if(row[indexType] == 'mouse') {
+      dataMousePage.addRow([  row[indexPageX]       ,    row[indexPageY]      ]);
+      dataMouseScreen.addRow([  row[indexScreenX]       ,    row[indexScreenY]      ]);
+    } else {
+      dataClickPage.addRow([row[indexPageX],row[indexPageY]]);
+      dataClickScreen.addRow([row[indexScreenX],row[indexScreenY]]);
+    }
+  }
+  
+  res.dataMousePage = dataMousePage;
+  res.dataMouseScreen = dataMouseScreen;
+  res.dataClickPage = dataClickPage;
+  res.dataClickScreen = dataClickScreen;
+  
+  return res;
+}
+
+function createChartUiApp() {
+  
+   dataObj = getDataTable();
+  
+   var mousePageChartBuilder = Charts.newScatterChart()
+       .setTitle('User Mouse Map(Global)')
+       .setXAxisTitle('Page X')
+       .setYAxisTitle('Page Y')
+       .setDimensions(500, 500)
+       .setLegendPosition(Charts.Position.NONE);
+   
+   var mouseScreenChartBuilder = Charts.newScatterChart()
+       .setTitle('User Mouse Map(Window)')
+       .setXAxisTitle('Screen X')
+       .setYAxisTitle('Screen Y')
+       .setDimensions(500, 500)
+       .setLegendPosition(Charts.Position.NONE);
+  
+  var clickPageChartBuilder = Charts.newScatterChart()
+       .setTitle('User Click Map(Global)')
+       .setXAxisTitle('Page X')
+       .setYAxisTitle('Page Y')
+       .setDimensions(500, 500)
+       .setLegendPosition(Charts.Position.NONE);
+  
+  var clickScreenChartBuilder = Charts.newScatterChart()
+       .setTitle('User Click Map(Window)')
+       .setXAxisTitle('Screen X')
+       .setYAxisTitle('Screen Y')
+       .setDimensions(500, 500)
+       .setLegendPosition(Charts.Position.NONE);
+   
+  
+  mousePageChartBuilder.setDataTable(dataObj.dataMousePage);
+  mouseScreenChartBuilder.setDataTable(dataObj.dataMouseScreen);
+  clickPageChartBuilder.setDataTable(dataObj.dataClickPage);
+  clickScreenChartBuilder.setDataTable(dataObj.dataClickScreen);
+  
+  var mousePageChart = mousePageChartBuilder.build();
+  var mouseScreenChart = mouseScreenChartBuilder.build();
+  var clickPageChart = clickPageChartBuilder.build();
+  var clickScreenChart = clickScreenChartBuilder.build();
+  
+  var app = UiApp.createApplication();
+  var vPanel = app.createVerticalPanel();
+  var hPanel1 = app.createHorizontalPanel();
+  var hPanel2 = app.createHorizontalPanel();
+  hPanel1.add(mousePageChart).add(mouseScreenChart);
+  hPanel2.add(clickPageChart).add(clickScreenChart);
+  vPanel.add(hPanel1).add(hPanel2);
+
+  app.add(vPanel);
+   
+   
+  return app;
+   
+   //var flow = app.createFlowPanel();
+   
+   //var document.getElementById('columnchart')
+   //return UiApp.createApplication().add(chart);
+}
+
+
+/**
+
+**/
+function createChart(srcURL) {
+  var chartBuilder = Charts.newScatterChart()
+  .setTitle('Adjusted GDP vs. U.S. Population')
+  .setXAxisTitle('U.S. Population (millions)')
+  .setYAxisTitle('Adjusted GDP ($ billions)')
+  .setDimensions(1000, 1000)
+  .setLegendPosition(Charts.Position.NONE)
+  .setDataSourceUrl(srcURL);
+  
+  return chartBuilder.build();
+}
 
 function doGet(e) { // change to doPost(e) if you are recieving POST data
-  /*var ss = SpreadsheetApp.openById(ScriptProperties.getProperty('active'));
+  
+  /*
+  var data = Charts.newDataTable()
+      .addColumn(Charts.ColumnType.STRING, "Name")
+      .addColumn(Charts.ColumnType.STRING, "Gender")
+      .addColumn(Charts.ColumnType.NUMBER, "Age")
+      .addColumn(Charts.ColumnType.NUMBER, "Donuts eaten")
+      .addRow(["Michael", "Male", 12, 5])
+      .addRow(["Michael", "Male", 12, 5])
+      .addRow(["Elisa", "Female", 20, 7])
+      .addRow(["Robert", "Male", 7, 3])
+      .addRow(["John", "Male", 54, 2])
+      .addRow(["Jessica", "Female", 22, 6])
+      .addRow(["Aaron", "Male", 3, 1])
+      .addRow(["Margareth", "Female", 42, 8])
+      .addRow(["Miranda", "Female", 33, 6])
+      .build();
+
+  var ageFilter = Charts.newNumberRangeFilter()
+      .setFilterColumnLabel("Age")
+      .build();
+
+  var genderFilter = Charts.newCategoryFilter()
+      .setFilterColumnLabel("Gender")
+      .build();
+
+  var pieChart = Charts.newPieChart()
+      .setDataViewDefinition(Charts.newDataViewDefinition()
+                            .setColumns([0,3]))
+      .build();
+
+  var tableChart = Charts.newTableChart()
+      .build();
+
+  var dashboard = Charts.newDashboardPanel()
+      .setDataTable(data)
+      .bind([ageFilter, genderFilter], [pieChart, tableChart])
+      .build();
+
+  var uiApp = UiApp.createApplication();
+
+  dashboard.add(uiApp.createVerticalPanel()
+                .add(uiApp.createHorizontalPanel()
+                    .add(ageFilter).add(genderFilter)
+                    .setSpacing(70))
+                .add(uiApp.createHorizontalPanel()
+                    .add(pieChart).add(tableChart)
+                    .setSpacing(10)));
+
+  uiApp.add(dashboard);
+  return uiApp;*/
+  
+  
+  
+  
+  
+  
+  
+
+  
+
+  
+  
+  
+  return createChartUiApp();
+  
+  /*
+  var ss = SpreadsheetApp.openById(ScriptProperties.getProperty('active'));
   var sheet = ss.getSheetByName("DATA");
   var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]; //read headers
   var nextRow = sheet.getLastRow(); // get next row
@@ -61,17 +265,17 @@ function doGet(e) { // change to doPost(e) if you are recieving POST data
     panel.add(app.createLabel(p +" "+e.parameters[p]));
   }
   app.add(panel);
-  return app;*/
+  return app;
   
   var app = UiApp.createApplication();
   var form = app.createFormPanel();
   var flow = app.createFlowPanel();
-  flow.add(app.createTextBox().setName("textBox"));
-  flow.add(app.createSubmitButton("Submit11111111"));
+  flow.add(app.createTextBox().setName(type));
+  flow.add(app.createSubmitButton(type));
   form.add(flow);
   app.add(form);
   Logger.log("just for asdf444444");
-  return app;
+  return app;*/
 }
 
 /**
